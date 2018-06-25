60a8d09764eda910177b32a6b5dbaaf4d2348a5f
Allen Zhang <alzhang@bccrc.ca>
2018-06-25 17:52:12 -0400

    Split up EM and VB cellassign versions, add in priors for EM

---
+++ R/cellassign_em.R
@@ -0,0 +1,168 @@
+#' Assign cells to known cell types
+#' @importFrom methods is
+#' @importFrom SummarizedExperiment assays
+#' @param exprs_obj SingleCellExperiment object
+#' @param rho Marker gene matrix (binary)
+#' @param s Numeric vector of size factors
+#' @param X Numeric matrix of auxiliary variables (NULL for none)
+#' @param exprs_obj_known SingleCellExperiment object for labeled data (semisupervised)
+#' @param s_known Size factors for labeled cells
+#' @param X_known Auxiliary variables for labeled cells
+#' @export
+cellassign_em <- function(exprs_obj,
+                          rho,
+                          s = NULL,
+                          X = NULL,
+                          exprs_obj_known = NULL,
+                          s_known = NULL,
+                          X_known = NULL,
+                          known_types = NULL,
+                          delta_log_prior_mean = 0,
+                          phi_log_prior_mean = 0,
+                          delta_log_prior_scale = 1,
+                          phi_log_prior_scale = 1,
+                          data_type = c("RNAseq", "MS"),
+                          n_batches = 1,
+                          rel_tol_adam = 1e-4,
+                          rel_tol_em = 1e-4,
+                          max_iter_adam = 1e5,
+                          max_iter_em = 20,
+                          learning_rate = 0.1,
+                          verbose = TRUE,
+                          sce_assay = "counts",
+                          phi_type = "global",
+                          gamma_init = NULL,
+                          num_runs = 1) {
+  
+  # Get expression input
+  Y <- extract_expression_matrix(exprs_obj, sce_assay = sce_assay)
+  
+  if (!is.null(exprs_obj_known)) {
+    Y0 <- extract_expression_matrix(exprs_obj_known, sce_assay = sce_assay)
+  } else {
+    Y0 <- matrix(nrow = 0, ncol = ncol(Y))
+  }
+  
+  # Check X is correct
+  if(!is.null(X)) {
+    if(!(is.matrix(X) && is.numeric(X))) {
+      stop("X must either be NULL or a numeric matrix")
+    }
+  }
+  
+  # Check X_known is correct
+  if(!is.null(X_known)) {
+    if(!(is.matrix(X_known) && is.numeric(X_known))) {
+      stop("X_known must either be NULL or a numeric matrix")
+    }
+  }
+  
+  
+  stopifnot(is.matrix(Y))
+  stopifnot(is.matrix(Y0))
+  stopifnot(is.matrix(rho))
+  
+  if(is.null(rownames(rho))) {
+    warning("No gene names supplied - replacing with generics")
+    rownames(rho) <- paste0("gene_", seq_len(nrow(rho)))
+  }
+  if(is.null(colnames(rho))) {
+    warning("No cell type names supplied - replacing with generics")
+    colnames(rho) <- paste0("cell_type_", seq_len(ncol(rho)))
+    
+    if (!is.null(known_types)) {
+      known_types <- paste0("gene_", known_types)
+    }
+  }
+  
+  N <- nrow(Y)
+  N0 <- nrow(Y0)
+  
+  X <- initialize_X(X, N, verbose = verbose)
+  X0 <- initialize_X(X_known, N0, verbose = verbose)
+  
+  G <- ncol(Y)
+  C <- ncol(rho)
+  P <- ncol(X)
+  
+  P0 <- ncol(X0)
+  
+  # Check the dimensions add up
+  stopifnot(nrow(X) == N)
+  stopifnot(nrow(rho) == G)
+  
+  stopifnot(nrow(X0) == N0)
+  
+  # Compute size factors for each cell
+  if (is.null(s)) {
+    message("No size factors supplied - computing from matrix. It is highly recommended to supply size factors calculated using the full gene set")
+    s <- scran::computeSumFactors(t(Y))
+  }
+  
+  if (is.null(s_known)) {
+    if (N0 > 0) {
+      message("No size factors supplied - computing from matrix. It is highly recommended to supply size factors calculated using the full gene set")
+      s_known <- scran::computeSumFactors(t(Y_known))
+    } else {
+      s_known <- numeric(0)
+    }
+  }
+  
+  if (any(!known_types %in% colnames(rho))) {
+    stop("Known types must be a proper subset of cluster names.")
+  }
+  gamma0 <- model.matrix(~ 0 + factor(known_types, levels = colnames(rho)))
+  
+  stopifnot(nrow(gamma0) == N0)
+  
+  res <- NULL
+  data_type <- match.arg(data_type)
+  
+  if (!is.null(gamma_init)) {
+    gamma_eps <- 1e-3
+    gamma_init[gamma_init < gamma_eps] <- gamma_eps
+    gamma_init <- gamma_init/rowSums(gamma_init)
+  }
+  
+  if(data_type == "RNAseq") {
+    run_results <- lapply(1:num_runs, function(i) {
+      # TODO: Only run 1 ADAM iteration per EM generation
+      res <- inference_tensorflow(Y = Y,
+                                  rho = rho,
+                                  s = s,
+                                  X = X,
+                                  G = G,
+                                  C = C,
+                                  N = N,
+                                  P = P,
+                                  Y0 = Y0,
+                                  s0 = s_known,
+                                  X0 = X0,
+                                  N0 = N0,
+                                  P0 = P0,
+                                  gamma0 = gamma0,
+                                  delta_log_prior_mean = delta_log_prior_mean,
+                                  phi_log_prior_mean = phi_log_prior_mean,
+                                  delta_log_prior_scale = delta_log_prior_scale,
+                                  phi_log_prior_scale = phi_log_prior_scale,
+                                  verbose = verbose,
+                                  n_batches = n_batches,
+                                  rel_tol_adam = rel_tol_adam,
+                                  rel_tol_em = rel_tol_em,
+                                  max_iter_adam = max_iter_adam,
+                                  max_iter_em = max_iter_em,
+                                  learning_rate = learning_rate,
+                                  phi_type = phi_type,
+                                  gamma_init = gamma_init)
+      
+      return(structure(res, class = "cellassign_fit"))
+    })
+    # Return best result
+    res <- run_results[[which.max(sapply(run_results, function(x) x$lls[length(x$lls)]))]]
+  } else if (data_type == "MS") {
+    # TODO: Implement GMM for MS data.
+    stop("Model for MS data not implemented at the moment.")
+  }
+  
+  return(res)
+}
--- R/inference-tensorflow.R
+++ R/inference-tensorflow.R
@@ -65,14 +65,14 @@ inference_tensorflow <- function(Y,
   } else if (phi_type == "cluster_specific") {
     phi_log <- tf$Variable(tf$random_uniform(shape(G,C), minval = -2, maxval = 2, seed = random_seed, dtype = tf$float64), dtype = tf$float64) #tf$zeros(shape(G,C))
     phi0_log <- tf$Variable(tf$random_uniform(shape(G,C), minval = -2, maxval = 2, seed = random_seed, dtype = tf$float64), dtype = tf$float64)
-    
+
     phi_log <- entry_stop_gradients(phi_log, tf$cast(rho_, tf$bool))
     phi0_log <- entry_stop_gradients(phi_log, tf$cast(rho_, tf$bool))
   }
-  
+
   beta <- tf$Variable(tf$random_normal(shape(G,P), mean = 0, stddev = 1, seed = random_seed, dtype = tf$float64), dtype = tf$float64)
   beta0 <- tf$Variable(tf$random_normal(shape(G,P0), mean = 0, stddev = 1, seed = random_seed, dtype = tf$float64), dtype = tf$float64)
-  
+
   #beta0 <- beta # testing
 
   # Stop gradient for irrelevant entries of delta_log
@@ -81,7 +81,7 @@ inference_tensorflow <- function(Y,
   # Transformed variables
   delta = tf$exp(delta_log)
   phi = tf$exp(phi_log)
-  
+
   phi0 = tf$exp(phi0_log)
 
   # Model likelihood
@@ -92,17 +92,17 @@ inference_tensorflow <- function(Y,
   mu_ngc = tf$add(tf$stack(base_mean_list, 2), tf$multiply(delta, rho_), name = "adding_base_mean_to_delta_rho")
   if (phi_type == "global") {
     mu_cng = tf$transpose(mu_ngc, shape(2,0,1))
-    
+
     mu_cng <- tf$exp(mu_cng)
-    
+
     p = mu_cng / (mu_cng + phi)
-    
+
     nb_pdf <- tfd$NegativeBinomial(probs = p, total_count = phi)
   } else if (phi_type == "cluster_specific") {
     mu_ngc <- tf$exp(mu_ngc)
-    
+
     p = mu_ngc / (mu_ngc + phi)
-    
+
     nb_pdf <- tfd$NegativeBinomial(probs = p, total_count = phi)
   }
 
@@ -110,11 +110,11 @@ inference_tensorflow <- function(Y,
   for(c in seq_len(C)) Y_tensor_list[[c]] <- Y_
   if (phi_type == "global") {
     Y__ = tf$transpose(tf$stack(Y_tensor_list, axis = 2), shape(2,0,1))
-    
+
     y_log_prob <- nb_pdf$log_prob(Y__)
   } else if (phi_type == "cluster_specific") {
     Y__ = tf$stack(Y_tensor_list, axis = 2)
-    
+
     y_log_prob_raw <- nb_pdf$log_prob(Y__)
     y_log_prob <- tf$transpose(y_log_prob_raw, shape(2,0,1))
   }
@@ -135,17 +135,17 @@ inference_tensorflow <- function(Y,
   mu0_ngc = tf$add(tf$stack(base_mean0_list, 2), tf$multiply(delta, rho_), name = "adding_base_mean_to_delta_rho_supervised")
   if (phi_type == "global") {
     mu0_cng = tf$transpose(mu0_ngc, shape(2,0,1))
-    
+
     mu0_cng <- tf$exp(mu0_cng)
-    
+
     p0 = mu0_cng / (mu0_cng + phi0)
-    
+
     nb_pdf0 <- tfd$NegativeBinomial(probs = p0, total_count = phi0)
   } else if (phi_type == "cluster_specific") {
     mu0_ngc <- tf$exp(mu0_ngc)
-    
+
     p0 = mu0_ngc / (mu0_ngc + phi0)
-    
+
     nb_pdf0 <- tfd$NegativeBinomial(probs = p0, total_count = phi0)
   }
 
@@ -153,11 +153,11 @@ inference_tensorflow <- function(Y,
   for(c in seq_len(C)) Y0_tensor_list[[c]] <- Y0_
   if (phi_type == "global") {
     Y0__ = tf$transpose(tf$stack(Y0_tensor_list, axis = 2), shape(2,0,1))
-    
+
     y0_log_prob <- nb_pdf0$log_prob(Y0__)
   } else if (phi_type == "cluster_specific") {
     Y0__ = tf$stack(Y0_tensor_list, axis = 2)
-    
+
     y0_log_prob_raw <- nb_pdf0$log_prob(Y0__)
     y0_log_prob <- tf$transpose(y0_log_prob_raw, shape(2,0,1))
   }
@@ -169,17 +169,17 @@ inference_tensorflow <- function(Y,
 
   Q1 = -tf$einsum('nc,cng->', gamma_fixed, y_log_prob)
   Q0 = -tf$einsum('nc,cng->', gamma_known, y0_log_prob)
-  
+
   ## Priors
-  delta_log_prior <- tfd$Normal(loc = tf$constant(delta_log_prior_mean, dtype = tf$float64), 
+  delta_log_prior <- tfd$Normal(loc = tf$constant(delta_log_prior_mean, dtype = tf$float64),
                                 scale = tf$constant(delta_log_prior_scale, dtype = tf$float64))
-  phi_log_prior <- tfd$Normal(loc = tf$constant(phi_log_prior_mean, dtype = tf$float64), 
+  phi_log_prior <- tfd$Normal(loc = tf$constant(phi_log_prior_mean, dtype = tf$float64),
                               scale = tf$constant(phi_log_prior_scale, dtype = tf$float64))
   delta_log_prob <- -tf$reduce_sum(delta_log_prior$log_prob(delta_log))
   phi_log_prob <- -tf$reduce_sum(phi_log_prior$log_prob(phi_log))
-  
+
   # TODO: Consider whether phi0 deserves as prior
-  
+
   ## End priors
 
   Q = Q1 + Q0 + delta_log_prob + phi_log_prob
@@ -218,19 +218,19 @@ inference_tensorflow <- function(Y,
       } else {
         g <- sess$run(gamma, feed_dict = fd)
       }
-        
+
       # M-step
       gfd <- dict(Y_ = Y[splits[[b]], ], X_ = X[splits[[b]], , drop = FALSE], s_ = s[splits[[b]]], rho_ = rho, Y0_ = Y0, X0_ = X0, s0_ = s0, gamma_known = gamma0, gamma_fixed = g)
 
       Q_old <- sess$run(Q, feed_dict = gfd)
       Q_diff <- rel_tol_adam + 1
       mi = 0
-      
+
       while(mi < max_iter_adam && Q_diff > rel_tol_adam) {
         mi <- mi + 1
-        
+
         sess$run(train, feed_dict = gfd)
-        
+
         if(mi %% 20 == 0) {
           if (verbose) {
             message(paste(mi, sess$run(Q1, feed_dict = gfd), sess$run(Q0, feed_dict = gfd), sep = " "))
